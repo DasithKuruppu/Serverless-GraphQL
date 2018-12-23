3f400a7c738c2cc8116908c515f28e3678176137
unknown <dasithkuruppu@gmail.com>
2018-12-23 21:06:32 +0530

    Fixed tests failing case

--- handler.ts
+++ handler.ts
@@ -1,22 +1,25 @@
 /* @flow */
 
 import schema from "./schemas/index";
-import { graphql, ExecutionResult} from "graphql";
+import { graphql } from "graphql";
 import { APIGatewayProxyEvent, Context, ProxyCallback } from "aws-lambda";
 // Highly scalable FaaS architecture :)=
 // Export a function which would be hooked up to the the λ node/ nodes as specified on serverless.yml template
-export function queryEvents(
+export async function queryEvents(
   event: APIGatewayProxyEvent,
   context: Context,
   callback: ProxyCallback,
-): void {
-  const parsedRequestBody = JSON.parse(event.body);
-  graphql(schema, parsedRequestBody.query, null, null, parsedRequestBody.variables, parsedRequestBody.operationName)
-    .then((result: ExecutionResult): void => {
-        return callback(null, { statusCode: 200, body: JSON.stringify(result) });
-      },
-    )
-    .catch((err) => {
-      return callback(err);
-    });
+): Promise<void> {
+  const parsedRequestBody = (event && event.body) ? JSON.parse(event.body) : {};
+  try {
+    const result = await graphql(schema,
+      parsedRequestBody.query,
+      null,
+      null,
+      parsedRequestBody.variables,
+      parsedRequestBody.operationName);
+    callback(null, { statusCode: 200, body: JSON.stringify(result) });
+  } catch (error) {
+    callback(error);
+  }
 }
