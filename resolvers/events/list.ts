c7db414feed2b3e3c229ff564d8b898aad2cfbd6
Dasith Kuruppu <DasithK@embla.lk>
2018-12-24 10:41:48 +0530

    Added at field now resolves to proper date when queried

--- resolvers/events/list.ts
+++ resolvers/events/list.ts
@@ -4,4 +4,7 @@ export default () =>
   dynamoDb
     .scan({ TableName: process.env.TABLE_NAME })
     .promise()
-    .then((list: DynamoDB.DocumentClient.ScanOutput) => list.Items);
+    .then((list: DynamoDB.DocumentClient.ScanOutput) => list.Items.map(
+    (Item) => {
+        return ({ ...Item, addedAt: new Date(Item.addedAt) });
+      }));
